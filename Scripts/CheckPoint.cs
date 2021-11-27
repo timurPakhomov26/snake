31c4b1ce6ada60b9e14b7517b71a2efb1a24e3e9
pzdcTIP26 <timurpahomo26@gmail.com>
2021-11-27 17:11:24 +0300

    release

---
+++ Scripts/CheckPoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{   
+   [SerializeField] private MeshRenderer _checkPointMesh;
+   private MeshRenderer _player;
+
+    private void Start()
+    {
+      
+    }
+    private void Update()
+    {
+          _player = FindObjectOfType<Snake>().GetComponent<MeshRenderer>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            _player.material.color = _checkPointMesh.material.color;
+        }
+    }
+}
